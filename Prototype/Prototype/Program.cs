cb25f385a39eefca03d14f63f68697571c37e893
Mask <rebootroot13@gmail.com>
2020-09-26 15:30:40 +0300

    Realisation the prototype pattern

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prototype
+{
+    interface IPrototype<U> 
+    {
+        public U Clone();
+    }
+
+    class Psina<T>:IPrototype<Psina<T>>
+    {
+        
+        public int blohi = 120;
+        public bool sluni = true;
+        public T Zhratva { get; set; } = default;
+
+        public Psina<T> Clone()
+        {
+            return (Psina<T>)this.MemberwiseClone(); 
+            //var Zhratva = new Zhratva {gram };
+           // 
+
+
+          //  return new Psina<T>(blohi, sluni);
+        }
+    }
+
+    class Pedigree
+    {
+        public Pedigree() : this(1000, "koshka", "oblozhka") { }
+        
+        public Pedigree(int gramm, string vkus, string someinfo)
+        {
+            this.gramm = gramm;
+            this.vkus = vkus;
+            this.someinfo = someinfo;
+        }
+
+        public int gramm;
+        public string vkus;
+        public string someinfo;
+
+    }
+
+    class Program
+    {
+        
+    }
+
+}
